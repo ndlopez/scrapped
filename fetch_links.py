a8c437139aa42790b9dc6d84d695fbdedf11d597
Diego <ndzerglink@live.jp>
2023-08-06 21:01:19 +0900

    Modified fetch_links

--- fetch_links.py
+++ fetch_links.py
@@ -1,3 +1,6 @@
+"""
+    Write json file every time this app is called
+"""
 import json
 
 down_data = {}
@@ -5,10 +8,12 @@ down_data = {}
 down_data["consult_time"] = "2023-08-06 20:39:43"
 down_data["request_date"] = "2023/07/27"
 pdf_array = []
+aux_dic = {}
+aux_dic["title"] ="Television Room - Engines Are Starting"
+aux_dic["link"]="https://cdn-albums.tunein.com/gn/NJ4KL637GTg.jpg"
+pdf_array.append(aux_dic)
 down_data["downloaded"] = pdf_array
 
 outfile = r"down_data.json"
-with open(outfile,"a",encoding='utf8') as fp:
+with open(outfile,"w",encoding='utf8') as fp:
     json.dump(down_data,fp,indent=4)
-
-
